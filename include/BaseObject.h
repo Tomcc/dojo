96d5e82b14cc69333d72ead177b427efd22663e1
tommo89 <devnull@localhost>
2011-01-22 14:20:45 +0100

    aggiunti BaseObject e il common header (scordati i file)

---
+++ include/BaseObject.h
@@ -0,0 +1,16 @@
+#ifndef BaseObject_h__
+#define BaseObject_h__
+
+#include "dojo_common_header.h"
+
+namespace Dojo 
+{
+	class BaseObject
+	{
+	public:
+		
+	protected:
+	};
+}
+
+#endif
---
+++ include/dojo_common_header.h
@@ -0,0 +1,30 @@
+#ifndef dojo_common_header
+#define dojo_common_header
+
+#include "dojo_config.h"
+
+#include <assert.h>
+
+#include <string>
+#include <iostream>
+#include <map>
+
+#include <OpenGLES/ES1/gl.h>
+#include <OpenGLES/ES1/glext.h>
+
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/EAGLDrawable.h>
+
+#import <UIKit/UIKit.h>
+#import <UIKit/UIWindow.h>
+#import <UIKit/UIApplication.h>
+#import <UIKit/UIView.h>
+
+#import <QuartzCore/QuartzCore.h>
+
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+
+#import <Foundation/NSString.h>
+
+#endif
