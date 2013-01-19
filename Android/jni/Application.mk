d1e28fd84d668c64ab81e79e7639ced73e31a300
gabriele <gabriele@ubuntu>
2013-01-19 10:34:25 -0800

    Dojo make lib

--- Android/jni/Application.mk
+++ Android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := gnustl_static
-APP_MODULES := androiddojo
+APP_MODULES := dojo-android
 APP_ABI := armeabi
 #armeabi-v7a
 APP_PLATFORM := android-10
--- Android/jni/dojo/Android.mk
+++ Android/jni/dojo/Android.mk
@@ -4,12 +4,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 
-LOCAL_MODULE := androiddojo
+LOCAL_MODULE := dojo-android
 LOCAL_ARM_MODE := arm
 
 #paths
-DIP_INCLUDE_PATH := ../../../dependencies/include
-DIP_LIB_PATH := $(LOCAL_PATH)/../../../dependencies/lib
+DIP_INCLUDE_PATH := ../../../dependencies-android/include
+DIP_LIB_PATH := $(LOCAL_PATH)/../../../dependencies-android/lib
 DOJO_PATH_INCLUDE := ../../../include
 DOJO_PATH_INCLUDE_DOJO := ../../../include/dojo
 DOJO_PATH_ANDROID_INCLUDE_DOJO := ../../../include/dojo/android
@@ -40,24 +40,16 @@ LOCAL_C_INCLUDES+=$(LOCAL_PATH)/$(DIP_INCLUDE_PATH)\
 DOJO_CPP:=$(wildcard $(LOCAL_PATH)/$(DOJO_PATH_CPP)/*.cpp)
 DOJO_CPP+=$(wildcard $(LOCAL_PATH)/$(DOJO_PATH_ANDROID_CPP)/*.cpp)
 DOJO_C:=$(wildcard $(LOCAL_PATH)/$(DOJO_PATH_ANDROID_CPP)/*.c)
-#c/cpp game files
-DOJO_GAME_CPP:=$(wildcard $(LOCAL_PATH)/$(DOJO_GAME_PATH_ANDROID)/*.cpp)
-DOJO_GAME_C:=$(wildcard $(LOCAL_PATH)/$(DOJO_GAME_PATH_ANDROID)/*.c)
 
 #h files
 DOJO_H:=$(wildcard $(LOCAL_PATH)/$(DOJO_PATH_INCLUDE_DOJO)/*.h)
 DOJO_H+=$(wildcard $(LOCAL_PATH)/$(DOJO_PATH_ANDROID_INCLUDE_DOJO)/*.h)
-#h game files
-DOJO_GAME_H:=$(wildcard $(LOCAL_PATH)/$(DOJO_GAME_PATH_ANDROID)/*.h)
 
 
 #add cpp file
 LOCAL_SRC_FILES:=$(DOJO_CPP:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES+=$(DOJO_C:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES+=$(DOJO_H:$(LOCAL_PATH)/%=%)
-LOCAL_SRC_FILES+=$(DOJO_GAME_CPP:$(LOCAL_PATH)/%=%)
-LOCAL_SRC_FILES+=$(DOJO_GAME_C:$(LOCAL_PATH)/%=%)
-LOCAL_SRC_FILES+=$(DOJO_GAME_H:$(LOCAL_PATH)/%=%)
 				
 #openGL extra propriety
 LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS -DDEF_SET_OPENGL_ES2 -DGL_GLEXT_PROTOTYPES -DCP_USE_DOUBLES=0
@@ -66,7 +58,7 @@ LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS -DDEF_SET_OPENGL_ES2 -DGL_GLEXT_PROTOTYPES
 //-std=c++11
 LOCAL_CPPFLAGS   += -std=gnu++11 -frtti -fexceptions
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 #Build native glue static library as well.
 $(call import-module,android/native_app_glue)
