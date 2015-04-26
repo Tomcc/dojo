0de78ef304f9efba90a5a3337d3fa9c68121268f
Tommaso Checchi <tommaso.checchi1@gmail.com>
2015-04-26 02:09:21 +0200

    modernized Random, removed inconsistent Seed size

--- include/dojo/Random.h
+++ include/dojo/Random.h
@@ -25,22 +25,25 @@
 
 #include "Timer.h"
 
+#include <array>
+
 namespace Dojo {
 	///A Random implementation using Mersenne Twister	
 	class Random {
 		// Data
 	public:
-		typedef size_t Seed; //seed is 64 bits on 64 bits platforms
-		typedef unsigned int uint32; // unsigned 32 bit integer type
+		typedef uint64_t Seed;
 
 		enum { N = 624 }; // length of state vector
 		enum { SAVE = N + 1 }; // length of array for save()
 
+		typedef std::array<uint32_t, N> BigSeed;
+
 	protected:
 		enum { M = 397 }; // period parameter
 
-		uint32 state[N]; // internal state
-		uint32* pNext; // next value to get from state
+		uint32_t state[N]; // internal state
+		uint32_t* pNext; // next value to get from state
 		int left; // number of values left before reload needed
 
 	public:
@@ -52,7 +55,7 @@ namespace Dojo {
 		Random(Seed oneSeed);
 
 		///Creates a new Random generator using a big seed in an array
-		Random(uint32*const bigSeed, uint32 const seedLength = N);
+		Random(const BigSeed& seed);
 
 		Random(const Random& o);
 
@@ -63,9 +66,9 @@ namespace Dojo {
 		// Access to 32-bit random numbers
 
 		/// integer in [0,2^32-1]
-		uint32 randInt();
+		uint32_t randInt();
 		/// integer in [0,n] for n < 2^32
-		uint32 randInt(const uint32 n);
+		uint32_t randInt(const uint32_t n);
 
 		int randInt(int min, int max) {
 			return (int)(rand() * (max - min)) + min;
@@ -100,12 +103,12 @@ namespace Dojo {
 
 		/// Re-seeding functions with same behavior as initializers
 		void seed(Seed s);
-		void seed(uint32*const bigSeed, const uint32 seedLength = N);
+		void seed(const BigSeed& seed);
 		void seed();
 
 		/// Saving and loading generator state
-		void save(uint32* saveArray) const; // to array of size SAVE
-		void load(uint32*const loadArray); // from such array
+		void save(uint32_t* saveArray) const; // to array of size SAVE
+		void load(uint32_t*const loadArray); // from such array
 		friend std::ostream& operator<<(std::ostream& os, const Random& Random);
 		friend std::istream& operator>>(std::istream& is, Random& Random);
 		Random& operator=(const Random& o);
@@ -114,49 +117,49 @@ namespace Dojo {
 		void initialize(Seed oneSeed);
 		void reload();
 
-		uint32 hiBit(const uint32 u) const {
+		uint32_t hiBit(const uint32_t u) const {
 			return u & 0x80000000UL;
 		}
 
-		uint32 loBit(const uint32 u) const {
+		uint32_t loBit(const uint32_t u) const {
 			return u & 0x00000001UL;
 		}
 
-		uint32 loBits(const uint32 u) const {
+		uint32_t loBits(const uint32_t u) const {
 			return u & 0x7fffffffUL;
 		}
 
-		uint32 mixBits(const uint32 u, const uint32 v) const {
+		uint32_t mixBits(const uint32_t u, const uint32_t v) const {
 			return hiBit(u) | loBits(v);
 		}
 
-		uint32 magic(const uint32 u) const {
+		uint32_t magic(const uint32_t u) const {
 			return loBit(u) ? 0x9908b0dfUL : 0x0UL;
 		}
 
-		uint32 twist(const uint32 m, const uint32 s0, const uint32 s1) const {
+		uint32_t twist(const uint32_t m, const uint32_t s0, const uint32_t s1) const {
 			return m ^ (mixBits(s0, s1) >> 1) ^ magic(s1);
 		}
 
-		static uint32 hash(time_t t, clock_t c);
+		static uint32_t hash(time_t t, clock_t c);
 	};
 
 	// Functions are defined in order of usage to assist inlining
 
-	inline Random::uint32 Random::hash(time_t t, clock_t c) {
-		// Get a uint32 from t and c
-		// Better than uint32(x) in case x is floating point in [0,1]
+	inline uint32_t Random::hash(time_t t, clock_t c) {
+		// Get a uint32_t from t and c
+		// Better than uint32_t(x) in case x is floating point in [0,1]
 		// Based on code by Lawrence Kirby (fred@genesis.demon.co.uk)
 
-		static uint32 differ = 0; // guarantee time-based seeds will change
+		static uint32_t differ = 0; // guarantee time-based seeds will change
 
-		uint32 h1 = 0;
+		uint32_t h1 = 0;
 		unsigned char* p = (unsigned char *) &t;
 		for (size_t i = 0; i < sizeof(t); ++i) {
 			h1 *= UCHAR_MAX + 2U;
 			h1 += p[i];
 		}
-		uint32 h2 = 0;
+		uint32_t h2 = 0;
 		p = (unsigned char *) &c;
 		for (size_t j = 0; j < sizeof(c); ++j) {
 			h2 *= UCHAR_MAX + 2U;
@@ -170,10 +173,10 @@ namespace Dojo {
 		// See Knuth TAOCP Vol 2, 3rd Ed, p.106 for multiplier.
 		// In previous versions, most significant bits (MSBs) of the seed affect
 		// only MSBs of the state array.  Modified 9 Jan 2002 by Makoto Matsumoto.
-		register uint32* s = state;
-		register uint32* r = state;
+		register uint32_t* s = state;
+		register uint32_t* r = state;
 		register int i = 1;
-		*s++ = (uint32)seed & 0xffffffffUL;
+		*s++ = (uint32_t)seed & 0xffffffffUL;
 		for (; i < N; ++i) {
 			*s++ = (1812433253UL * (*r ^ (*r >> 30)) + i) & 0xffffffffUL;
 			r++;
@@ -184,7 +187,7 @@ namespace Dojo {
 		// Generate N new values in state
 		// Made clearer and faster by Matthew Bellew (matthew.bellew@home.com)
 		static const int MmN = int(M) - int(N); // in case enums are unsigned
-		register uint32* p = state;
+		register uint32_t* p = state;
 		register int i;
 		for (i = N - M; i--; ++p)
 			*p = twist(p[M], p[0], p[1]);
@@ -196,26 +199,28 @@ namespace Dojo {
 	}
 
 	inline void Random::seed(Seed s) {
-		// Seed the generator with a simple uint32
+		// Seed the generator with a simple uint32_t
 		initialize(s);
 		reload();
 	}
 
-	inline void Random::seed(uint32*const bigSeed, const uint32 seedLength) {
-		// Seed the generator with an array of uint32's
+	inline void Random::seed(const BigSeed& seed) {
+		DEBUG_ASSERT(seed.size() > 0, "Invalid seed");
+
+		// Seed the generator with an array of uint32_t's
 		// There are 2^19937-1 possible initial states.  This function allows
 		// all of those to be accessed by providing at least 19937 bits (with a
-		// default seed length of N = 624 uint32's).  Any bits above the lower 32
+		// default seed length of N = 624 uint32_t's).  Any bits above the lower 32
 		// in each element are discarded.
 		// Just call seed() if you want to get array from /dev/urandom
 		initialize(19650218UL);
 		register int i = 1;
-		register uint32 j = 0;
-		register int k = (N > seedLength ? N : seedLength);
+		register uint32_t j = 0;
+		register int k = (N > seed.size() ? N : seed.size());
 		for (; k; --k) {
 			state[i] =
 				state[i] ^ ((state[i - 1] ^ (state[i - 1] >> 30)) * 1664525UL);
-			state[i] += (bigSeed[j] & 0xffffffffUL) + j;
+			state[i] += (seed[j] & 0xffffffffUL) + j;
 			state[i] &= 0xffffffffUL;
 			++i;
 			++j;
@@ -223,7 +228,7 @@ namespace Dojo {
 				state[0] = state[N - 1];
 				i = 1;
 			}
-			if (j >= seedLength)
+			if (j >= seed.size())
 				j = 0;
 		}
 		for (k = N - 1; k; --k) {
@@ -244,20 +249,20 @@ namespace Dojo {
 	inline Random::Random() {
 		//use an high-precision timer to grab microseconds
 		Timer t;
-		seed((uint32)(t.currentTime() * 1000000));
+		seed((uint32_t)(t.currentTime() * 1000000));
 	}
 
 	inline Random::Random(Seed oneSeed) {
 		seed(oneSeed);
 	}
 
-	inline Random::Random(uint32*const bigSeed, const uint32 seedLength) {
-		seed(bigSeed, seedLength);
+	inline Random::Random(const BigSeed& bigSeed) {
+		seed(bigSeed);
 	}
 
 	inline Random::Random(const Random& o) {
-		register const uint32* t = o.state;
-		register uint32* s = state;
+		register const uint32_t* t = o.state;
+		register uint32_t* s = state;
 		register int i = N;
 		for (; i--; *s++ = *t++) {
 		}
@@ -265,7 +270,7 @@ namespace Dojo {
 		pNext = &state[N - left];
 	}
 
-	inline Random::uint32 Random::randInt() {
+	inline uint32_t Random::randInt() {
 		// Pull a 32-bit integer from the generator state
 		// Every other access function simply transforms the numbers extracted here
 
@@ -273,7 +278,7 @@ namespace Dojo {
 			reload();
 		--left;
 
-		register uint32 s1;
+		register uint32_t s1;
 		s1 = *pNext++;
 		s1 ^= (s1 >> 11);
 		s1 ^= (s1 << 7) & 0x9d2c5680UL;
@@ -281,11 +286,11 @@ namespace Dojo {
 		return (s1 ^ (s1 >> 18));
 	}
 
-	inline Random::uint32 Random::randInt(const uint32 n) {
+	inline uint32_t Random::randInt(const uint32_t n) {
 		/*
 		// Find which bits are used in n
 		// Optimized by Magnus Jonsson (magnus@smartelectronix.com)
-		uint32 used = n;
+		uint32_t used = n;
 		used |= used >> 1;
 		used |= used >> 2;
 		used |= used >> 4;
@@ -293,14 +298,14 @@ namespace Dojo {
 		used |= used >> 16;
 
 		// Draw numbers until one is found in [0,n]
-		uint32 i;
+		uint32_t i;
 		do
 			i = randInt() & used;  // toss unused bits to shorten search
 		while( i > n );
 		return i;*/
 
 		//HACK-  much faster while property-changing
-		return (uint32)rand((double)n);
+		return (uint32_t)rand((double)n);
 	}
 
 	inline double Random::rand() {
@@ -328,7 +333,7 @@ namespace Dojo {
 	}
 
 	inline double Random::rand53() {
-		uint32 a = randInt() >> 5, b = randInt() >> 6;
+		uint32_t a = randInt() >> 5, b = randInt() >> 6;
 		return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0); // by Isaku Wada
 	}
 
@@ -350,18 +355,18 @@ namespace Dojo {
 		return rand();
 	}
 
-	inline void Random::save(uint32* saveArray) const {
-		register const uint32* s = state;
-		register uint32* sa = saveArray;
+	inline void Random::save(uint32_t* saveArray) const {
+		register const uint32_t* s = state;
+		register uint32_t* sa = saveArray;
 		register int i = N;
 		for (; i--; *sa++ = *s++) {
 		}
 		*sa = left;
 	}
 
-	inline void Random::load(uint32*const loadArray) {
-		register uint32* s = state;
-		register uint32* la = loadArray;
+	inline void Random::load(uint32_t*const loadArray) {
+		register uint32_t* s = state;
+		register uint32_t* la = loadArray;
 		register int i = N;
 		for (; i--; *s++ = *la++) {
 		}
@@ -370,7 +375,7 @@ namespace Dojo {
 	}
 
 	inline std::ostream& operator<<(std::ostream& os, const Random& r) {
-		register const Random::uint32* s = r.state;
+		register const uint32_t* s = r.state;
 		register int i = r.N;
 		for (; i--; os << *s++ << "\t") {
 		}
@@ -378,7 +383,7 @@ namespace Dojo {
 	}
 
 	inline std::istream& operator>>(std::istream& is, Random& r) {
-		register Random::uint32* s = r.state;
+		register uint32_t* s = r.state;
 		register int i = r.N;
 		for (; i--; is >> *s++) {
 		}
@@ -390,8 +395,8 @@ namespace Dojo {
 	inline Random& Random::operator=(const Random& o) {
 		if (this == &o)
 			return (*this);
-		register const uint32* t = o.state;
-		register uint32* s = state;
+		register const uint32_t* t = o.state;
+		register uint32_t* s = state;
 		register int i = N;
 		for (; i--; *s++ = *t++) {
 		}
