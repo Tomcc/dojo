2d609bad29ac897a065e5f5187c8bd741ffe8dc2
Tommaso Checchi <tommaso.checchi1@gmail.com>
2016-03-26 23:01:27 +0100

    added a ring buffer that I didn't really need but duh it's done now

---
+++ include/dojo/ring_buffer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace Dojo {
+	template<class T>
+	class ring_buffer {
+	public:
+		explicit ring_buffer(size_t maxSize) : m_max_size(maxSize) {
+			m_buffer.reserve(maxSize);
+			m_buffer.shrink_to_fit(); //the C++ standard doesn't say if reserve can overallocate
+		}
+
+		size_t max_size() const {
+			return m_max_size;
+		}
+
+		auto peek_next() const {
+			if(m_write_idx < m_buffer.size()) {
+				return m_buffer.begin() + m_write_idx;
+			}
+			return m_buffer.end();
+		}
+
+		auto begin() {
+			return m_buffer.begin();
+		}
+
+		auto end() {
+			return m_buffer.end();
+		}
+
+		template <class... Args>
+		void emplace(Args&& ... args) {
+			if (m_write_idx == m_buffer.size()) {
+				m_buffer.emplace_back(std::forward<Args>(args)...);
+			}
+			else {
+				m_buffer[m_write_idx] = T(std::forward<Args>(args)...);
+			}
+
+			m_write_idx = (m_write_idx + 1) % max_size();
+		}
+
+	protected:
+		std::vector<T> m_buffer;
+		size_t m_write_idx = 0;
+		size_t m_max_size;
+	};
+}
+
